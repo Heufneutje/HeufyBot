d08aad947402c572cb4b338dfccb19e9ae876eed
Heufneutje <stefan.frijters@hotmail.com>
2013-06-26 15:49:22 +0200

    Forgot to update the version number

--- src/org/pircbotx/impl/HeufyBotStarter.java
+++ src/org/pircbotx/impl/HeufyBotStarter.java
@@ -27,7 +27,7 @@ public class HeufyBotStarter
     	  bot = new HeufyBot(true);
       }
       
-      bot.setVersion("HeufyBot IRC Bot V1.4.7 (PircBotX 1.9)");
+      bot.setVersion("HeufyBot IRC Bot V1.4.8 (PircBotX 1.9)");
     }
     catch (Exception e)
     {
